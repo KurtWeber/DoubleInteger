3636a33e33a1db99b96cfdeb640eb49d0430e6f7
KurtWeber <weberkrt@msn.com>
2016-09-14 21:57:13 -0400

    Slide 5 method completed

---
+++ DoubleInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer between (1-100): ");
+            int integer = int.Parse(Console.ReadLine());
+            Console.WriteLine("Double {0} is {1}", integer, Double(integer));
+        }
+        static int Double(int integer)
+        {
+            int dblInteger = integer * 2;
+            return dblInteger;
+        }
+    }
+}
